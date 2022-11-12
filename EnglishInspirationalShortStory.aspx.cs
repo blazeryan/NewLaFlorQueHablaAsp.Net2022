f3f9276b5430181584caea50ecec9c72a2a6be1e
Ryan Wayne Blaisdell <blazeryan21@gmail.com>
2022-11-12 11:51:13 -0500

    Assigned selectedShortStoryID variable with results of query string. Results is Selected Short Story ID from a query returned from CreateCommand() method.

--- EnglishInspirationalShortStory.aspx.cs
+++ EnglishInspirationalShortStory.aspx.cs
@@ -19,18 +19,26 @@ namespace LaFlorQueHablaWebApplication
 
         protected void InspirationalShortStoryListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string selectedShortStoryID;
 
-
-            // Test Print to Output Window
-            System.Diagnostics.Debug.WriteLine(InspirationalShortStoryListBox.Text);
+            // Test Print to Output Window Prints only selected text, does not return ID
+            // System.Diagnostics.Debug.WriteLine("Test Print to Output Window " + InspirationalShortStoryListBox.Text);
 
             // Call Method to Create connection 
             // CreateCommand("Select [Short Story ID] From [Short Story] Where [Short Story Name] = '" + InspirationalShortStoryListBox.Text + "';", "Data Source=DESKTOP-D98SK4H;Initial Catalog=LaFlorQueHablaDB;Integrated Security=True");
 
             // Call Method to Create connection "This is a cleaner way of writing the above code without using concatonation. This uses interpolation.
-            CreateCommand($"Select [Short Story ID] From [Short Story] Where [Short Story Name] = '{InspirationalShortStoryListBox.Text}';", "Data Source=DESKTOP-D98SK4H;Initial Catalog=LaFlorQueHablaDB;Integrated Security=True");
+            //CreateCommand($"Select [Short Story ID] From [Short Story] Where [Short Story Name] = '{InspirationalShortStoryListBox.Text}';", "Data Source=DESKTOP-D98SK4H;Initial Catalog=LaFlorQueHablaDB;Integrated Security=True");
+
+
+            // Call CreateCommand() Method to Create connection "This is a cleaner way of writing the above code without using concatonation. This uses string interpolation. Then this statement places query result being returned from the CreateCommand() method into the selectedShortStoryID variable.
+            selectedShortStoryID = CreateCommand($"Select [Short Story ID] From [Short Story] Where [Short Story Name] = '{InspirationalShortStoryListBox.Text}';", "Data Source=DESKTOP-D98SK4H;Initial Catalog=LaFlorQueHablaDB;Integrated Security=True");
+
+            // Test Print selectedShortStoryID value. Prints selected short story ID that was returned from query in CreateCommand() method.
+            System.Diagnostics.Debug.WriteLine("This is the Selected Short Story ID " + selectedShortStoryID);
+
         }
-   
+
 
 
 
@@ -51,23 +59,38 @@ namespace LaFlorQueHablaWebApplication
 
 
         // Method to Create Connection to Database
-        private static void CreateCommand(string queryString,
+        private static string CreateCommand(string query,
     string connectionString)
-        {
+            
+        {   
+            string selectedIDQueryResult = "";
+
+
             using (SqlConnection connection = new SqlConnection(
                        connectionString))
             {
+
                 connection.Open();
 
-                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlCommand command = new SqlCommand(query, connection);
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        System.Diagnostics.Debug.WriteLine(String.Format("{0}", reader[0]));
+                        // Test Print Query Result
+                        // System.Diagnostics.Debug.WriteLine(String.Format("{0}", reader[0]));
+                        
+                        // Place query result into selectedIDQueryResult variable
+                        selectedIDQueryResult = String.Format("{0}", reader[0]);
+
                     }
+
+                   
                 }
             }
+
+            // Return variable that holds query result
+            return selectedIDQueryResult;
         }
 
 
